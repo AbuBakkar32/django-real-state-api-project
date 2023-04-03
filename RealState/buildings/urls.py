f1e455ef40b2e3337f377413a28592aa49caa397
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-04-03 17:59:12 +0600

    home model created with view

--- RealState/buildings/urls.py
+++ RealState/buildings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import HomeListAPIView, HomeDetailsAPIView, ImageView
+from .views import HomeListAPIView, HomeDetailsAPIView, ImageView, Search
 
 urlpatterns = [
     path('', HomeListAPIView.as_view(), name="list"),
     path('<slug>', HomeDetailsAPIView.as_view(), name="home-details"),
     path('images/<pk>', ImageView.as_view(), name="images"),
+    path('search/', Search.as_view(), name="search"),
 ]
--- RealState/buildings/views.py
+++ RealState/buildings/views.py
@@ -28,3 +28,30 @@ class ImageView(APIView):
         images = home.images.all()
         serializer = ImageFilesSerializer(images, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class Search(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        queryset_list = Home.objects.all()
+        # City
+        if 'city' in request.data:
+            city = request.data['city']
+            if city:
+                queryset_list = queryset_list.filter(city__icontains=city)
+
+        # State
+        if 'state' in request.data:
+            state = request.data['state']
+            if state:
+                queryset_list = queryset_list.filter(state__icontains=state)
+
+        # Bedrooms
+        if 'bedrooms' in request.data:
+            bedrooms = request.data['bedrooms']
+            if bedrooms:
+                queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+        serializer = HomeSerializer(queryset_list, many=True)
+        return Response(serializer.data)
