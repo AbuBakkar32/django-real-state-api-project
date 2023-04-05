d095cb1d9c88e9c1bfb20921351c4dcbffa758bd
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-04-05 12:39:28 +0600

    setting updated

--- RealState/RealState/settings.py
+++ RealState/RealState/settings.py
@@ -108,14 +108,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
         # 'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
 
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework.authentication.BasicAuthentication',
-    #     'rest_framework.authentication.SessionAuthentication',
-    #
-    #     'rest_framework.authentication.TokenAuthentication',
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ]
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
 }
 
 # Internationalization
